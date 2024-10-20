9b8fec7d896730aafb119878283d07a50dd24c1f
Ritik <40895801+ritikvirus@users.noreply.github.com>
2024-10-20 18:33:41 +0530

    Update api.py

--- api.py
+++ api.py
@@ -15,5 +15,5 @@ def run_command():
         return jsonify({'error': e.output.decode()}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=7860)
 
